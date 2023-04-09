b09bceb4a283e6e5afa0f6bb6890544492661ec3
jacobgunlock <jacobkgundlach@gmail.com>
2023-04-09 14:10:29 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import mainImg from './assets/smash-photo-tag.jpg'
+
+function App() {
+  return (
+    <div className="App">
+     <img src={mainImg} alt="smash characters"/>
+    </div>
+  );
+}
+
+export default App;
