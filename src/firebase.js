2441b8f2304b0083318ede966cca34054f3a11ef
jacobgunlock <112229170+jacobgunlock@users.noreply.github.com>
2025-11-18 10:49:13 -0600

    Add comments for Firebase API key configuration
    Added comments regarding API key usage.

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,10 @@ import { getFirestore} from 'firebase/firestore'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+// ************
+// API KEY MEANT TO BE HARDCODED; AS PART OF 'TOP' LESSON INSTRUCTIONS 
+// ************
 const firebaseConfig = {
   apiKey: "AIzaSyCCJpCRmptZ6aR1mGaNrVkQbtSAqPYjHVM",
   authDomain: "smash-photo-tag.firebaseapp.com",
